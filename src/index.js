c3c71138413dad9f221af306441ab5de818cb6d4
Cristian Peñarrieta <cristian.penarrieta@gmail.com>
2018-01-13 21:44:27 -0800

    adding react-router and redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const About = () => <div>about</div>
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={App}/>
+      <Route exact path="/about" component={About}/>
+    </div>
+  </Router>, 
+  document.getElementById('root')
+)
+registerServiceWorker()
